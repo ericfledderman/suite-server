828ed16e5a5bb653bed87b530bb54263cdf50ba0
Eric Fledderman <ericfledderman@me.com>
2024-11-08 21:33:19 -0500

    Update main.js

--- functions/starter/src/main.js
+++ functions/starter/src/main.js
@@ -7,7 +7,7 @@ export default async ({ req, res, log, error }) => {
     .setProject(process.env.APPWRITE_FUNCTION_PROJECT)
     .setKey(req.headers['X-Suite-Key'] ?? '')
 
-  const uers = new Users(client)
+  const users = new Users(client)
 
   try {
     const response = await users.list()
@@ -27,42 +27,3 @@ export default async ({ req, res, log, error }) => {
     getInspired: 'https://builtwith.appwrite.io'
   })
 }
-
-
-
-
-// import { Client, Users } from 'node-appwrite';
-
-// // This Appwrite function will be executed every time your function is triggered
-// export default async ({ req, res, log, error }) => {
-//   // You can use the Appwrite SDK to interact with other services
-//   // For this example, we're using the Users service
-//   const client = new Client()
-//     .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
-//     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-//     .setKey(req.headers['x-appwrite-key'] ?? '');
-//   const users = new Users(client);
-
-//   try {
-//     const response = await users.list();
-//     // Log messages and errors to the Appwrite Console
-//     // These logs won't be seen by your end users
-//     log(`Total users: ${response.total}`);
-//   } catch(err) {
-//     error("Could not list users: " + err.message);
-//   }
-
-//   // The req object contains the request data
-//   if (req.path === "/ping") {
-//     // Use res object to respond with text(), json(), or binary()
-//     // Don't forget to return a response!
-//     return res.text("Pong");
-//   }
-
-//   return res.json({
-//     motto: "Build like a team of hundreds_",
-//     learn: "https://appwrite.io/docs",
-//     connect: "https://appwrite.io/discord",
-//     getInspired: "https://builtwith.appwrite.io",
-//   });
-// };
