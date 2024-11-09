db34c128e9104c7387d4e8b67f80d4ae942030ad
Eric Fledderman <ericfledderman@me.com>
2024-11-08 22:08:28 -0500

    Update main.js

--- functions/starter/src/main.js
+++ functions/starter/src/main.js
@@ -9,17 +9,27 @@ export default async ({ res, res, log, error }) => {
 
   const functions = new Functions(client)
 
+  let response
+
+  try {
+    response = await functions.list()
+    log(`Functions: ${ response.functions }`)
+  } catch (err) {
+    error('Could not list users: ' + err.message)
+  }
+
   if (req.path === '/v1/functions') {
-    if (req.method === 'GET') {
-      try {
-        const reponse = await functions.list()
-        log(`Functions: ${ response.functions }`)
-        return res.text(response.functions)
-      } catch (err) {
-        error('Could not list users: ' + err.message)
-      }
+    if(req.method === 'GET') {
+      return res.json(response)
     }
   }
+
+  return res.json({
+    motto: 'Build like a team of hundreds_',
+    learn: 'https://appwrite.io/docs',
+    connect: 'https://appwrite.io/discord',
+    getInspired: 'https:/builtwith.appwrite.io'
+  })
 }
 
 
