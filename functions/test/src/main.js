3770cf1ada6f1ff8afc10920c49bfa6f05aa934d
Eric Fledderman <ericfledderman@me.com>
2024-11-08 21:08:36 -0500

    Update main.js

--- functions/test/src/main.js
+++ functions/test/src/main.js
@@ -6,12 +6,12 @@ const port = 3000
 api.use(express.json())
 
 api.get('/api/hello', (req, res) => {
-  res.status(200).json({ message: 'Hello from Express on Appwrite Functions!' })
+  res.json({ message: 'Hello from Express on Appwrite Functions!' })
 })
 
 export default async (req, res) => {
   if (req.url === '/api/hello' && req.method === 'GET') {
-    res.status(200).json({ mesage: 'Hello from Express on Appwrite Functions!' })
+    res.json({ mesage: 'Hello from Express on Appwrite Functions!' })
   } else {
     res.status(404).send('Not Found')
   }
